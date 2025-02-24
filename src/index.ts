0fb0912e0143e91905b2e6ba75d411c6aa869a88
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-24 18:33:04 +0400

    setting up prisma and  making schema

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
 
 app.use('/api/auth', authRoutes)
