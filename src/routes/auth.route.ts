94a770cc09f7736b58592f811825c3ef56c695c2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-02-24 18:06:38 +0400

    working on auth controller

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,18 +1,13 @@
 import express from 'express'
+import { login, logout, signup } from '../controllers/auth.controller.js'
 
 const router= express.Router()
 
-router.get('/login', (req, res) => {
-  res.send('Logged in successfully')
-})
+router.post('/login', login)
 
-router.get('/logout', (req, res) => {
-  res.send('Logged out successfully')
-})
+router.post('/logout', logout)
 
-router.get('/signup', (req, res) => {
-  res.send('Signed up successfully')
-})
+router.post('/signup', signup)
 
 
 
