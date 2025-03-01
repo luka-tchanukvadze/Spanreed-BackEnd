f0b60d1868edb6a83392039b7d95ff6034a1b8e8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-01 20:58:53 +0400

    implementing signup function

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,62 @@
 import { Request, Response } from "express"
+import prisma from "../db/prisma.js"
+import bcrypt from "bcryptjs"
 
-export const signup = async(req:Request, res:Response) => {}
+export const signup = async(req:Request, res:Response) => {
+  try {
+
+    const {fullName, username, password, confirmPassword, gender} = req.body
+
+    if(!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({error: 'Please fill in all fileds'})
+    }
+
+    if(password !== confirmPassword) {
+      return res.status(400).json({error: "Passwords don't match"})
+    }
+
+    const user = await prisma.user.findUnique({ where: {username}})
+
+    if(user) {
+      return res.status(400).json({error: 'Username already exists'})
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName, 
+        username, 
+        password: hashedPassword, 
+        gender,
+        profilePic: gender === 'male' ? boyProfilePic: girlProfilePic,
+      }
+    })
+
+    if(newUser) {
+      // generate toekn
+
+      res.status(201).json({
+        id: newUser.id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        boyProfilePic: newUser.profilePic
+      })
+    } else{
+      res.status(400).json({error: 'Invalid user data'})
+    }
+    
+  } catch (error:any) {
+    console.log("Error in signup controller",error.message)
+    res.status(500).json({
+      error: 'Internal Server Error'
+    })
+  }
+}
 
 export const login = async(req:Request, res:Response) => {
 
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
 app.use('/api/auth', authRoutes)
+
 app.use('/api/messages', messageRoutes)
 
 app.listen(5000, () => {
